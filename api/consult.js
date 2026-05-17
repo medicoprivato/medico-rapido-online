9f26d5edd1b36ce694f1a9ec80866d4337e825b0
medicoprivato <168222859+medicoprivato@users.noreply.github.com>
2026-05-17 14:40:20 +1000

    Update consult.js

--- api/consult.js
+++ api/consult.js
@@ -1,4 +1,4 @@
-exporexport default async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Metodo non consentito" });
   }
